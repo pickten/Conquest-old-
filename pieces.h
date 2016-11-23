674579790d115bdcfe710f3d4bcfe2952f4642f9
pickten <pickten@users.noreply.github.com>
2016-11-23 18:50:05 -0500

    Minor changes;
    I only made this commit to be able to make an additional branch;
    just fixes everything to use RBTrees instead of arrays

--- pieces.h
+++ pieces.h
@@ -2,40 +2,61 @@
 #ifndef PIECES_H
 #define PIECES_H
 
+
+
 struct pieceType;
 struct piece;
 struct pieceAction;
 struct nodeAction;
 
+// time to run the rbtree nonsense.
+// I know what I'm doing, I swear.
+#define RBTREE_TYPE intPiece
+#include "rbtrees.h"
+#define RBTREES_H_intPiece 1
+#undef RBTREE_TYPE
+
+#define RBTREE_TYPE pieceType
+#include "rbtrees.h"
+#define RBTREES_H_pieceType 1
+#undef RBTREE_TYPE
+
+#define RBTREE_TYPE charWrapper
+#include "rbtrees.h"
+#define RBTREES_H_charWrapper 1
+#undef RBTREE_TYPE
+
 struct intPiece {
   int num;
-  struct piece* piece;
+  struct pieceType* piece;
+};
+
+struct charWrapper {
+  char c;
 };
 
   
 
 struct intPieceAction {
   bool defaultState;
-  int exceptionCount;
-  struct intPiece* exceptions;
+  struct RBTree_intPiece* exceptions;
 };
 
   
 
 struct pieceAction {
-  int exceptionCount;
   bool defaultState;
-  struct pieceType* exceptions;
+  struct RBTree_pieceType* exceptions;
 };
 
 
 struct nodeAction {
-  int exceptionCount;
   bool defaultState;
-  char* exceptions;  /* since node types are chars */
+  struct RBTree_charWrapper* exceptions;  /* since node types are chars */
 };
 
-  
+
+
 
 struct pieceType {
   int maxMoves;  /* how many moves the piece can take in one turn */
@@ -71,10 +92,11 @@ struct piece {
 
   struct pieceType* pieceType;
 
-  struct piece* piecesCarried;
-
-  int numberPiecesCarried;
+  struct RBTree_piece* piecesCarried;
   
 };
+
+
+
 
 #endif  /* PIECES_H */

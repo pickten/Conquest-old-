53cd55565cb920954e158509b27ab36e135282d7
PlatyPrograms <PlatyPrograms@gmail.com>
2016-11-23 18:09:11 -0500

    bug fixes

--- mapinit.h
+++ mapinit.h
@@ -1,15 +1,14 @@
 typedef struct Node {
-<<<<<<< HEAD
-  struct Node * adj0 = NULL;
-  struct Node * adj1 = NULL;
-  struct Node * adj2 = NULL;
-  struct Node * adj3 = NULL;
+  struct Node * adj0;
+  struct Node * adj1;
+  struct Node * adj2;
+  struct Node * adj3;
   /* I've defined four adj nodes just in case there is a board that
    * needs that many. Each node has pointers to its adjacent nodes purely for the
    * purpose of checking validity of moves without too much mucking.
    */
-  piece * currentPiece = NULL; /* Whatever piece is on the node */
-  char type; /* This will either be L (land) or S (sea)
+  // piece * currentPiece; /* Whatever piece is on the node */
+  char *type; /* This will either be L (land) or S (sea)
 	      * Pl treated as land and Ps as water for the purpose of moving, but there are other 
 	      * checks that might be useful regarding port/land distinctions.
 	      */

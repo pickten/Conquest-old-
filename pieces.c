f481127f058b8418b8c9e028413dc265c537215b
pickten <pickten@users.noreply.github.com>
2016-11-24 23:02:34 -0500

    Add new/empty/delete for piece related stuff

--- pieces.c
+++ pieces.c
@@ -1,5 +1,138 @@
 #include "pieces.h"
-#define DOESTHIS(name, type) bool can ## name ## Raw(struct pieceType *x, int id){                     \
+
+
+void deletePiece(struct piece *p){
+  free(p->piecesCarried);
+  free(p);
+}
+
+void deletePieceType(struct pieceType *p){
+  free(p->Carry);
+  free(p->Combo);
+  free(p->DismountAttack);
+  free(p->Kill);
+  free(p->MountAttack);
+  free(p->Recapture);
+  free(p->Travel);
+  free(p);
+  
+}
+
+void deleteIntPiece(struct intPiece *p){
+  free(p->piece);
+  free(p);
+  
+}
+
+void deletePieceAction(struct pieceAction *p){
+  free(p->exceptions);
+  free(p);
+}
+
+void deleteIntPieceAction(struct intPieceAction *p){
+  free(p->exceptions);
+  free(p);
+}
+
+void deleteNodeAction(struct nodeAction *p){
+  free(p->exceptions);
+  free(p);
+}
+
+void deleteCharWrapper(struct charWrapper *p){
+  free(p);
+}
+
+
+struct piece* newPiece(int id, struct pieceType *p) {
+  struct piece *a = malloc(sizeof(struct piece));
+  if(a == NULL)
+    return NULL;
+  a->id = id;
+  a->pieceType = p;
+  a->piecesCarried = emptyRBTree_piece();
+  return a;
+  
+}
+
+struct pieceType* newPieceType(int id){
+  struct pieceType *a = malloc(sizeof(struct pieceType));
+  if(a == NULL)
+    return NULL;
+  a->id = id;
+  return a;
+}
+
+struct intPiece* newIntPiece(int i, struct pieceType *p){
+  struct intPiece *a = malloc(sizeof(struct intPiece));
+  if(a == NULL)
+    return NULL;
+  a->num = i;
+  a->piece = p;
+  return a;
+}
+
+struct pieceAction* newPieceAction(bool d, struct RBTree_pieceType *p){
+  struct pieceAction *a = malloc(sizeof(struct pieceAction));
+  if(a == NULL)
+    return NULL;
+  a->defaultState = d;
+  a->exceptions = p;
+  return a;
+}
+
+struct intPieceAction* newIntPieceAction(bool d, struct RBTree_intPiece *p){
+  struct intPieceAction *a = malloc(sizeof(struct intPieceAction));
+  if(a == NULL)
+    return NULL;
+  a->defaultState = d;
+  a->exceptions = p;
+  return a;
+}
+
+struct nodeAction* newNodeAction(bool d, struct RBTree_charWrapper *p){
+  struct nodeAction  *a = malloc(sizeof(struct nodeAction));
+  if(a == NULL)
+    return NULL;
+  a->defaultState = d;
+  a->exceptions = p;
+  return a;
+}
+
+
+struct charWrapper* newCharWrapper(char c){
+  struct charWrapper  *a = malloc(sizeof(struct charWrapper));
+  if(a == NULL)
+    return NULL;
+  a->c = c;
+  return a;
+}
+
+struct intPiece* emptyIntPiece(struct pieceType *p){
+  return newIntPiece(1, p);
+}
+
+struct pieceAction* emptyPieceAction(bool d){
+  return newPieceAction(d, emptyRBTree_pieceType());
+}
+
+struct intPieceAction* emptyIntPieceAction(bool d){
+  return newIntPieceAction(d, emptyRBTree_intPiece());
+}
+
+struct nodeAction* emptyNodeAction(bool d){
+  return newNodeAction(d, emptyRBTree_charWrapper());
+}
+
+struct charWrapper* emptyCharWrapper(void){
+  return newCharWrapper('\0');
+}
+
+
+
+
+
+#define DOESTHIS(name, type) bool can ## name ## Raw(struct pieceType *x, int id){ \
     return (AFFIX(findRB, type)(x->name->exceptions, id) != NULL) ^ (x->name->defaultState);}
 
 
@@ -12,3 +145,12 @@ DOESTHIS(MountAttack, pieceType);
 DOESTHIS(DismountAttack, pieceType);
 
 #undef DOESTHIS
+
+void startCarrying(struct piece *p, struct piece *o){
+  insertRB_piece(p->piecesCarried, o);
+}
+
+void stopCarrying(struct piece *p, struct piece *o){
+  removeRB_piece(p->piecesCarried, o->id);
+}
+
--- pieces.h
+++ pieces.h
@@ -26,6 +26,12 @@ struct nodeAction;
 #define RBTREES_H_charWrapper 1
 #undef RBTREE_TYPE
 
+#define RBTREE_TYPE piece
+#include "rbtrees.h"
+#define RBTREES_H_piece 1
+#undef RBTREE_TYPE
+
+
 struct intPiece {
   int num;
   struct pieceType* piece;
@@ -70,13 +76,9 @@ struct pieceType {
   
 
   struct pieceAction* Combo;
-
   struct intPieceAction* Recapture;
-
   struct nodeAction* Travel;
-
   struct pieceAction* MountAttack;
-
   struct pieceAction* DismountAttack;
   
 };
@@ -90,12 +92,36 @@ struct piece {
   
 
   int movesLeft;
-
   struct pieceType* pieceType;
-
   struct RBTree_piece* piecesCarried;
-  
 };
+
+
+
+void deletePiece(struct piece *p);
+void deletePieceType(struct pieceType *p);
+void deleteIntPiece(struct intPiece *p);
+void deletePieceAction(struct pieceAction *p);
+void deleteIntPieceAction(struct intPieceAction *p);
+void deleteNodeAction(struct nodeAction *p);
+void deleteCharWrapper(struct charWrapper *p);
+
+
+struct piece* newPiece(int id, struct pieceType *p);
+struct pieceType* newPieceType(int id);
+struct intPiece* newIntPiece(int i, struct pieceType *p);
+struct pieceAction* newPieceAction(bool d, struct RBTree_pieceType *p);
+struct intPieceAction* newIntPieceAction(bool d, struct RBTree_intPiece *p);
+struct nodeAction* newNodeAction(bool d, struct RBTree_charWrapper *p);
+struct charWrapper* newCharWrapper(char c);
+
+
+struct intPiece* emptyIntPiece(struct pieceType *p);
+struct pieceAction* emptyPieceAction(bool d);
+struct intPieceAction* emptyIntPieceAction(bool d);
+struct nodeAction* emptyNodeAction(bool d);
+struct charWrapper* emptyCharWrapper(void);
+
 
 
 #define CANDOTHIS(name, type) bool can ## name ## Raw(struct pieceType *x, int id)
@@ -110,4 +136,9 @@ CANDOTHIS(MountAttack, pieceType);
 CANDOTHIS(DismountAttack, pieceType);
 
 #undef CANDOTHIS
+
+void startCarrying(struct piece *p, struct piece *o);
+void stopCarrying(struct piece *p, struct piece *o);
+
+
 #endif  /* PIECES_H */
